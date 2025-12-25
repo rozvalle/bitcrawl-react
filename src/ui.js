3322448b44b35c041bd3c59dd13efb95b79283fe
Rozvalle <elmovaleros11@gmail.com>
2025-12-25 21:30:22 +0800

    refactor: remove unused button click sound function from ui.js

--- src/ui.js
+++
@@ -1,7 +0,0 @@
-// src/ui.js
-import { zzfx } from "./audio/zzfxMicro.js"; // ⚡ import your zzfx function
-
-// Play a small click sound for buttons
-export function buttonClickSound() {
-    zzfx(0.2, 0.5, 400); // volume, decay, frequency
-}
